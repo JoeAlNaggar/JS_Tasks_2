66010ecbebf65adb3ae9991008989cdc5fddadd3
JoeAlNaggar <joealnagar@gmail.com>
2023-08-30 06:33:05 +0300

    first commit

---
+++ Circle_Area/script.js
@@ -0,0 +1,3 @@
+var userRadius = parseFloat(prompt("Enter the radius of the circle : "));
+var circleArea = Math.pow(userRadius,2) * Math.PI ;
+alert("The total area of the circle is : " + circleArea.toFixed(2));
---
+++ Square_Root/script.js
@@ -0,0 +1,3 @@
+var userValue = parseFloat(prompt("Enter the value that you want to calculate its square root : "));
+var valueSquareRoot = Math.sqrt(userValue);
+alert("Square root of " + userValue + " is : " + valueSquareRoot.toFixed(2));
